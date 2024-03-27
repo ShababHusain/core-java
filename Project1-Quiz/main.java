f8961c2b344378264a6ca79275d3f7e5f6bb1af8
Shabab Husain <81950972+shababAster@users.noreply.github.com>
2024-03-27 16:13:57 +0530

    Add files via upload

---
+++ Project1-Quiz/main.java
@@ -0,0 +1,11 @@
+
+public class main 
+{
+   
+    public static void main(String[] args)
+    {
+        QuestionService service = new QuestionService();
+        service.playQuiz();
+        service.printScore();
+    }
+}
